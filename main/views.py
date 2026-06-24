4c29ce414c74573e8ca35b791415b9d483fd579e
Farhanhamim1111 <140664923+Farhanhamim1111@users.noreply.github.com>
2026-06-24 00:00:30 -0400

    Adding more tabs

--- main/views.py
+++ main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(requests):
     return render(requests, 'home.html')
+
+def about(requests):
+    return render(requests, 'about.html')
+
+def resume(requests):
+    return render(requests, "resume.html")
+
+def contact(requests):
+    return render(requests, "contact.html")
